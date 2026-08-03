7618b3bff706f3f32c7d4bb5d2355cc9b86561b7
Eugen Hildt <eugen.hildt@gmail.com>
2026-08-04 00:54:36 +0200

    0023: dependency upgrades pinia eslint vite storybook

--- dashboard/src/components/shared/ui/drop-down/helpers/strip-html.helper.ts
+++ dashboard/src/components/shared/ui/drop-down/helpers/strip-html.helper.ts
@@ -1,4 +1,3 @@
 export function stripHtml(html: string): string {
-  // eslint-disable-next-line sonarjs/slow-regex
-  return html.replace(/<[^>]*>/g, '');
+  return html.replace(/<[^<>]*>/g, '');
 }
